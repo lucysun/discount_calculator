c525eb6830506d19d4fb4edca2195a793c6dfaa8
Lucy Sun <lucy.a.sun@gmail.com>
2014-06-07 17:35:59 -0400

    raises error if discount percentage > 100%

--- disc_calc.py
+++ disc_calc.py
@@ -1,6 +1,8 @@
 class DiscountCalculator(object):
 	def calculate(self, total, discount_amount, discount_type):
 		if discount_type == 'percent':
+			if discount_amount > 100:
+				raise ValueError('Percentage discount cannot exceed 100%')
 			percentage_discount = float(discount_amount) / 100
 			discount = float(total) * percentage_discount
 		elif discount_type == 'absolute':
